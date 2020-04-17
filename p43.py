776d19fa89ff270dab6662185a1db1fbc44eedf7
amingoltabar <61889677+amingoltabar@users.noreply.github.com>
2020-04-17 16:25:59 +0430

    Add files via upload

--- p43.py
+++ p43.py
@@ -1,26 +1,41 @@
 my_list=[]
+count=1
 print('Creating the list')
-while len(my_list)<7:
-    a=(input('Enter the integer: '))
+while len(my_list)<7: #creating the list
+    print('Enter the integer number',count,end='')
+    a=input(':')
     try:
         val=int(a)
         my_list.append(val)
+        count+=1
     except ValueError:
         try:
             val=float(a)
             print('You entered a float. ')
             pass
         except ValueError:
-            print('Your did not enter a number. ')
+            print('You did not enter a number. ')
             pass
-x=float(input('Enter the  index '))
-while x<0 or x>6 or x!=int(x):#Entered indexes must be integer and in the range of list.
-    x=float(input('Index you have entered is out of range or non-integer.Enter the index again. '))
-y=float(input('Enter the  index '))
-while y<0 or y>6 or y!=int(y):
-     y=float(input('Index you have entered is out of range or non-integer.Enter the index again. '))
-x=int(x)
-y=int(y)
+index_list=[]
+count_1=0
+while len(index_list)<2: #inputting indexes
+    print('Enter the index number',count_1+1,end='')
+    b=input(':')
+    try:
+        val=int(b)
+        if val<=6 and val>=0:
+            index_list.append(val)
+            count_1+=1
+        else:
+            print('Your entered number is not in range.')
+    except ValueError:
+        try:
+            val=float(b)
+            print('You entered a float.')
+        except ValueError:
+            print('You did not enter a number.')
+x=index_list[0]
+y=index_list[1]
 def calculateave(array,x,y):#This function receives 2 indexes and a list.Then calculates the average of elements between these 2 indexes.
     s=0
     count=0
@@ -30,7 +45,7 @@ def calculateave(array,x,y):#This function receives 2 indexes and a list.Then ca
             count+=1
         ave=s/count
     elif x>y:
-        for i in range(y,x+1,1):
+        for i in range(y,x+1,1):                                                             
             s+=array[i]
             count+=1
         ave=s/count
